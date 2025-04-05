f56c7e85360fd0643284b59ee8a6fb3e1f096408
sketchycrypt <sketchygfx@hotmail.com>
2025-04-05 23:38:29 +0000

    netlify fix?

---
+++ src/routes/+page.svelte
@@ -0,0 +1,10 @@
+<script>
+    import { page } from '$app/stores';
+    import { goto } from '$app/navigation';
+    
+    $: if ($page.status === 404) {
+      goto('/');
+    }
+  </script>
+  
+  <p>Redirecting to homepage...</p>
